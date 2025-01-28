1cedc4f0bc793f415442a9ce0847db2a4370acd9
Gorav <goravjindal86@gmail.com>
2025-01-28 12:15:01 +0530

    ai work done

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -10,7 +10,7 @@ const ImageAnalyzer = () => {
 
   // Initialize Gemini AI
   const genAI = new GoogleGenerativeAI('AIzaSyAzoZGqAFZiKAVS-Xvmb2PZedZrjXkO0RA'); // Replace with your API key
-  const model = genAI.gegtGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.GenerativeModel({ model: "gemini-1.5-flash" });
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
